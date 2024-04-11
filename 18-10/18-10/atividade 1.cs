36cb4f224d9fb0c39532acc4d40b9c50a44de585
Ana Laura Andrade <143561088+nalaurandrade@users.noreply.github.com>
2024-04-11 19:58:41 -0300

    Add files via upload

---
+++ 18-10/18-10/atividade 1.cs
@@ -0,0 +1,19 @@
+/******************************************************************************
+
+                            Online C# Compiler.
+                Code, Compile, Run and Debug C# program online.
+Write your code in this editor and press "Run" button to execute it.
+
+*******************************************************************************/
+
+using System;
+class Texto
+{
+    static void Main()
+    {
+        string data = "22";
+        string disciplina = "Algoritmo e Programação";
+        Console.WriteLine($"Hoje {data} outubro é a avaliação da disciplina: {disciplina}!");
+    }
+}
+
