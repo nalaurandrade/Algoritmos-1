14bc02fc52c0dc1bf053629759e994ce63016380
Ana Laura Andrade <143561088+nalaurandrade@users.noreply.github.com>
2024-04-11 21:21:01 -0300

    Add files via upload

---
+++ computer science.cs
@@ -0,0 +1,24 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelloWorld
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+            Console.WriteLine("Computer Science is no more about computers than satronomy is about telescopes");
+
+            Console.WriteLine("- Edsger Wybe Djkstra");
+
+
+            Console.ReadLine();
+
+        }
+    }
+}
